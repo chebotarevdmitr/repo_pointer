b99843c778ae8db4e7f8520f7c82d673376cd18a
DIM <4ebotarov_dim@inbox.ru>
2024-06-06 23:29:55 +0300

    int number

--- repo_pointer.cpp
+++ repo_pointer.cpp
@@ -3,20 +3,10 @@
 using namespace std;
 
 int main() {
-	int n = 5;
-	cout << n << endl;         //vivod  peremennoy n
-	cout << &n << endl;       // adress peremennoy n
-	int* ptr = &n;           //int* sozdan ukazantel / prt -> imya / na adres peremennoy n (&n)
-	cout << ptr << endl;    //vivod adressa  ukazanelya
-	cout << *ptr << endl;  //dostup k znacheniyu ptr chrez razimenovanie *ptr
-	*ptr = 10;            //izmenenie znacheniya na adrese *ptr
-	cout << *ptr << endl;
-	cout << n << endl;   //vivod peremennoy n s drugim znacheniem
+	int number = 5;
 
-	int v;
-	int* ptr2=&v;
-	*ptr2 = 7;
-	cout << "\t V= " << v << endl;
+
+
 
 	system("pause>0");
 	return 0;
