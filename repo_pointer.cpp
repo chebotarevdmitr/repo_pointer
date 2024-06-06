c12acc775d279e5b6339db83a342334027aafc72
DIM <4ebotarov_dim@inbox.ru>
2024-06-06 22:58:54 +0300

    vivod peremennoy n s drugim znacheniem

--- repo_pointer.cpp
+++ repo_pointer.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main() {
 	int n = 5;
-	cout << n << endl;//vivod  peremennoy n
-	cout << &n << endl;// adress peremennoy n
-	int* ptr = &n; //int* sozdan ukazantel / prt -> imya / na adres peremennoy n (&n)
+	cout << n << endl;     //vivod  peremennoy n
+	cout << &n << endl;   // adress peremennoy n
+	int* ptr = &n;       //int* sozdan ukazantel / prt -> imya / na adres peremennoy n (&n)
 	cout << ptr << endl;//vivod adressa  ukazanelya
 	cout << *ptr << endl;//dostup k znacheniyu ptr chrez razimenovanie *ptr
-	*ptr = 10;//izmenenie znacheniya na adrese *ptr
+	*ptr = 10;          //izmenenie znacheniya na adrese *ptr
 	cout << *ptr << endl;
-
+	cout << n << endl; //vivod peremennoy n s drugim znacheniem
 
 
 	system("pause>0");
