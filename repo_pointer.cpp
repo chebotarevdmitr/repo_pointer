82a64790fa0db266da372aa756e2b2110d26a33c
DIM <4ebotarov_dim@inbox.ru>
2024-06-06 22:34:55 +0300

    ukazatel na adres "&n"

--- repo_pointer.cpp
+++ repo_pointer.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 	int n = 5;
 	cout << &n << endl;// adress peremennoy n
+	int* ptr = &n; //int* sozdan ukazantel / prt -> imya / na adres peremennoy n (&n)
 
 	system("pause>0");
 	return 0;
