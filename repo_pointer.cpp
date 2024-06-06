a2d0b7c6a67d34ae563afe0e51e5ad1340e635ae
DIM <4ebotarov_dim@inbox.ru>
2024-06-06 22:46:47 +0300

    razimenovanie  *ptr

--- repo_pointer.cpp
+++ repo_pointer.cpp
@@ -8,6 +8,7 @@ int main() {
 	cout << &n << endl;// adress peremennoy n
 	int* ptr = &n; //int* sozdan ukazantel / prt -> imya / na adres peremennoy n (&n)
 	cout << ptr << endl;//vivod adressa  ukazanelya
+	cout << *ptr << endl;//dostup k znacheniyu ptr chrez razimenovanie *ptr
 	system("pause>0");
 	return 0;
 }
