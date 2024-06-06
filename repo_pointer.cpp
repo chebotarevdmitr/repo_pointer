fd230028d6f9a42e225d533b3d2c529b9b6223f5
DIM <4ebotarov_dim@inbox.ru>
2024-06-06 22:41:19 +0300

    vivod po ukazanelyu

--- repo_pointer.cpp
+++ repo_pointer.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main() {
 	int n = 5;
+	cout << n << endl;//vivod  peremennoy n
 	cout << &n << endl;// adress peremennoy n
 	int* ptr = &n; //int* sozdan ukazantel / prt -> imya / na adres peremennoy n (&n)
-
+	cout << ptr << endl;//vivod adressa  ukazanelya
 	system("pause>0");
 	return 0;
 }
